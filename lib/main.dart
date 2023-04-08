873c8e9de85566494802731454c0e89b68b11ad0
Yash Sharma <yashww07@gmail.com>
2023-04-08 10:00:35 +0530

    first project added

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flute/views/login_view.dart';
+import 'package:flute/views/register_view.dart';
+import 'package:flute/views/verify_email.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue, 
+      ),
+      home: const HomePage(),
+      routes: {
+        '/login/' : (context) => const LoginView(),
+        '/register/' : (context) => const RegisterView(),
+        '/notes/' : (context) => const NotesView(),
+      },
+    ),
+  );
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState){
+              case ConnectionState.done :
+                
+                  final user = FirebaseAuth.instance.currentUser;
+                  if(user != null){
+                    if(user.emailVerified){
+                      return const NotesView();
+                    }
+                    else{
+                      return const VerifyEmailView();
+                    }
+                  }else{
+                    return const LoginView();                    
+                  }
+                default :
+                return const CircularProgressIndicator();
+            }
+          },
+     );
+  }
+}
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected : (value) async {
+              switch (value) {
+
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/login/', 
+                      (_) => false,
+                      );
+                  }
+                  
+              }
+          },itemBuilder: (context){
+            return const[
+              PopupMenuItem<MenuAction>(
+                value: MenuAction.logout,  
+                child: Text('LOGOUT'),
+              ),
+            ];            
+          },
+          )
+        ],
+      ),
+    ); 
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context){
+  return showDialog<bool>(
+    context: context, 
+    builder: (context){
+      return AlertDialog(
+        title: const Text('Sign Out'),
+        content: const Text('Are you sure you want to sign out?'),
+        actions: [
+
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            }, 
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Logout'),
+          ),
+        ],
+      );
+    },
+    ).then((value) => value ?? false);
+}
+
+
+
+
+
+
+ 
---
+++ lib/views/login_view.dart
@@ -0,0 +1,99 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password; 
+
+  @override
+    void initState() {
+      _email = TextEditingController();
+      _password = TextEditingController();
+      super.initState();
+    }
+
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose(); 
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login'),
+      ),
+      body: Column(
+       children: [
+        TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Enter your email here',
+           ),
+        ),
+        TextField(
+              controller: _password,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration: const InputDecoration(
+                hintText: 'Enter your password here',
+               ),
+          ),
+          TextButton(
+             onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+                try{
+                  await FirebaseAuth.instance.signInWithEmailAndPassword(
+                     email: email, 
+                     password: password,
+                     );
+                     Navigator.of(context).pushNamedAndRemoveUntil(
+                        '/notes/', 
+                        (route) => false,
+                      );
+                          
+                 } on FirebaseAuthException catch(e) {
+                 if(e.code == 'user-not-found') {
+                    devtools.log('User Not Found');
+                  } else if (e.code == 'wrong-password'){
+                         devtools.log('Wrong Password');
+                    }
+                  }
+                  },
+                           
+                        child: const Text('LOGIN'), 
+                        ),
+             TextButton(
+              onPressed: () { 
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  '/register/', 
+                  (route) => false,
+                  );
+              }, 
+              child:  const Text('Not Registered yet? Register here!',
+              ),
+           )       
+        ],
+      ),
+    );                           
+  }
+}
+
+  
+
