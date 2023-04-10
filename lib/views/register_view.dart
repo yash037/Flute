5b2b6856597b282c6ee0e7a1b937dda4760c8ea2
yash037 <yashww07@gmail.com>
2023-04-10 09:57:34 +0530

    step-3

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flute/contants/routes.dart';
+import 'package:flute/utilities/show_error_dialog.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -62,21 +61,40 @@ late final TextEditingController _email;
                           final email = _email.text;
                           final password = _password.text;
                           try{
-                            final userCredential = 
-                              await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                            await FirebaseAuth.instance.createUserWithEmailAndPassword(
                                 email: email, 
                                 password: password,
                               );
-                              devtools.log('UserCredential');
-                        
+                              final user = FirebaseAuth.instance.currentUser;
+                              await user?.sendEmailVerification();
+                              Navigator.of(context).pushNamed(verifyEmailRoute);                        
                           }on FirebaseAuthException catch(e) {
                             if (e.code == 'weak-password'){
-                              devtools.log('Use Strong Password');
+                              await showErrorDialog(
+                                context, 
+                                'Use Strong Password'
+                              );
                             } else if (e.code == 'invalid-email'){
-                              devtools.log('Write Email in Correct Format');
+                              await showErrorDialog(
+                                context, 
+                                'This is an invalid email address'
+                              );
                             }else if (e.code == 'email-already-in-use'){
-                              devtools.log("Email is already in use.");
+                              await showErrorDialog(
+                                context, 
+                                'Email is already in use.'
+                              );
+                            } else{
+                              await showErrorDialog(
+                                context, 
+                                'Error ${e.code}',
+                                );
                             }
+                          } catch (e) {
+                            await showErrorDialog(
+                              context, 
+                              e.toString(),
+                              );
                           }
                              
     
--- lib/views/verify_email.dart
+++ lib/views/verify_email.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flute/contants/routes.dart';
 import 'package:flutter/material.dart';
 
 class VerifyEmailView extends StatefulWidget {
@@ -16,14 +17,25 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       ),
       body: Column(
         children: [
-          const Text('Please verify your email address :'),
+          const Text("We've sent you an email verification. Please open it to verify account."),
+          const Text("If you haven't receivde a verification email yet, press the button below"),
           TextButton(
             onPressed: () async {
             final user = FirebaseAuth.instance.currentUser;
             await user?.sendEmailVerification();
           }, 
           child: const Text('Send Email verification'),
-          )
+          ),
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                (route) => false,
+              );
+            }, 
+            child: const Text('Restart'),
+            ),
         ],
       ),
     );
